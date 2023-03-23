ffe48fae6587bd20ec7337880bcd2cb181fd1115
jamlol <justin.a@tamu.edu>
2023-03-23 18:11:07 -0500

    adding starting files

---
+++ Main/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void increment(String version){
+
+    }
+    public static void increment(String version, int index){
+
+    }
+
+}
