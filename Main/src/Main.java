85c64588f8ccf9b987a455fd0dbfc3b6da340939
jamlol <justin.a@tamu.edu>
2023-03-23 18:37:21 -0500

    completed increment with 1 parameter

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,10 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(increment("1.1.1"));
+
     }
 
-    public static void increment(String version){
+    public static String increment(String version){
+        //initialize result string
+        String result = "";
 
+        //split the version parameter string based on "." and index into array
+        String[] versionValues = version.split("\\.");
+
+        //go to last index and increment by 1.
+        versionValues[versionValues.length-1] = String.valueOf(Integer.parseInt(versionValues[versionValues.length-1])+1);
+
+        //add all values of array into result string
+        for(String a: versionValues){
+            result += a;
+        }
+
+        //add the "." between each number
+        result = result.replace("",".");
+
+        //remove the first and last characters which are extra ".".
+        result = result.substring(1,result.length()-1);
+
+        return result;
     }
     public static void increment(String version, int index){
 
